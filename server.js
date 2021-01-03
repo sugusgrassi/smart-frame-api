99d525f552f4821afe54796d459fbc9248489892
sugus.grassi <sugus.grassi@gmail.com>
2021-01-02 23:57:02 -0300

    adding connection

--- server.js
+++ server.js
@@ -13,10 +13,8 @@ const db = knex({
   // connect to your own database here
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'sugus',
-    password : '',
-    database : 'smart-brain'
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
   }
 });
 
